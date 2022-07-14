1d07a731131733172a6191ccc1268e248161e051
bsj2776 <bsj2776@naver.com>
2022-07-14 14:56:55 +0900

    iamport error 수정

--- lib/Profile.dart
+++ lib/Profile.dart
@@ -9,23 +9,8 @@ class Profile extends StatelessWidget{
         title: Text('My Profile'),
       ),
       body: const Center(
-        User user = FirebaseAuth.getInstance().getCurrentUser();
-        if (user != null) {
-          for (UserInfo profile : user.getProviderData()) {
-            // Id of the provider (ex: google.com)
-            String providerId = profile.getProviderId();
 
-            // UID specific to the provider
-            String uid = profile.getUid();
-
-            // Name, email address, and profile photo Url
-            String name = profile.getDisplayName();
-            String email = profile.getEmail();
-            Uri photoUrl = profile.getPhotoUrl();
-            }
-        }
-
-    ),
+      ),
     );
   }
 }
