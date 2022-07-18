4533fc4063282d3f9529e5b6eff2cc8566a575aa
mink0517 <93696682+mink0517@users.noreply.github.com>
2022-07-18 14:26:06 +0900

    shopping page modified.

--- lib/order/view/shopping_page.dart
+++ lib/order/view/shopping_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:univery_flutter_2022/order/controller/cart_controller.dart';
+import 'package:univery_flutter_2022/order/controller/shopping_controller.dart';
 
 class ShoppingPage extends StatelessWidget{
-  const ShoppingPage({Key? key}) : super(key: key);
+  ShoppingPage({Key? key}) : super(key: key);
+  final shoppingController = Get.put(ShoppingController());
+  final cartController = Get.put(CartController());//일단 controller가 디펜던시 인젝션 되면 다시 생성할 필요 없음
 
   @override
   Widget build(BuildContext context){
@@ -10,52 +15,83 @@ class ShoppingPage extends StatelessWidget{
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, index){
-                  return Card(
-                    margin: EdgeInsets.all(12),
-                    child: Padding(
-                      padding: EdgeInsets.all(16),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            child: GetX<ShoppingController>(
+              builder: (controller) {//이 컨트롤러를 통해서 모든 데이터 정보를 가져오게 됨
+                return ListView.builder(
+                    itemCount: controller.products.length,
+                    itemBuilder: (context, index){
+                      return Card(
+                        margin: EdgeInsets.all(12),
+                        child: Padding(
+                          padding: EdgeInsets.all(16),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.end,
                             children: [
-                              Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                                 children: [
-                                  Text('Product name',//card안 왼쪽 위 글자(칼럼 안)
-                                  style: TextStyle(fontSize: 24),
+                                  Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        '${controller.products[index].productName}',//card안 왼쪽 위 글자(칼럼 안)/string interpolation을 사용해서 controller를 불러오고
+                                        //뒤에 products를 불러와서 인덱스를 가져옴
+                                      style: TextStyle(fontSize: 24),
+                                      ),
+                                      Text('${controller.products[index].productDescription}'),//product밑 글자(칼럼 안)
+                                    ],
                                   ),
-                                  Text('Product description'),//product밑 글자(칼럼 안)
+                                  Text('\$${controller.products[index].price}',//card 오른쪽 위(row안)
+                                    style: TextStyle(fontSize: 24),
+                                  ),
                                 ],
                               ),
-                              Text('\$30',//card 오른쪽 위(row안)
-                                style: TextStyle(fontSize: 24),
+                              ElevatedButton(
+                                  onPressed: (){
+                                    cartController.addToItem(controller.products[index]);
+                                  },
+                                  child: Text('Add to cart'),//card안 오른쪽 아래 버튼/ add to cart버튼을 누를때마다 해당아이템의 가격이 합산되어서 출력됨
                               ),
                             ],
                           ),
-                          ElevatedButton(
-                              onPressed: (){},
-                              child: Text('Add to cart'),//card안 오른쪽 아래 버튼
-                          ),
-                        ],
-                      ),
-                    ),
+                        ),
+                      );
+                    },
                   );
-                },
-              ),
+              }
+            ),
           ),
           SizedBox(
             height: 30,
           ),
-          Text('Show Total amount', style: TextStyle(//화면 맨 아래 가운데 글자
-            fontSize: 25,
-            color: Colors.white
-          ),)
+          GetX<CartController>(
+            builder: (controller) {
+              return Text('Total amount: \$ ${controller.totalPrice}',
+                style: TextStyle(//화면 맨 아래 가운데 글자
+                fontSize: 25,
+                color: Colors.white),
+              );
+            }
+          ),
+          SizedBox(
+            height: 100,
+          )
         ],
+      ),
+      floatingActionButton: FloatingActionButton.extended(
+          onPressed: (){},
+          label: GetX<CartController>(
+            builder: (controller) {
+              return Text(
+                cartController.count.toString(),
+                style: TextStyle(
+                fontSize: 20,
+      ),
+     );
+            }
+          ),
+      icon: Icon(Icons.add_shopping_cart_rounded),
+      backgroundColor: Colors.black87
       ),
     );
   }
