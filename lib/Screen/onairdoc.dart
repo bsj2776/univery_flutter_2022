839f7e7414276fbb752878a7b8f7376f0a677944
Luvxy <93637069+Luvxy@users.noreply.github.com>
2022-07-21 15:41:07 +0900

    edit onairdoc

--- lib/Screen/onairdoc.dart
+++ lib/Screen/onairdoc.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:iamport_flutter/iamport_payment.dart';
 import 'package:univery_flutter_2022/order/controller/cart_controller.dart';
 import 'package:univery_flutter_2022/order/controller/shopping_controller.dart';
 
@@ -13,6 +14,13 @@ class onairdoc extends StatefulWidget {
 class _onairdocState extends State<onairdoc> {
   final shoppingController = Get.put(ShoppingController());
 
+  String name = '홍길동';
+  String place1 = '매점';
+  String place2 = '은혜관';
+  String temName = '아이스크림';
+  String price = '1000원';
+  String hope = '아이스크림 안녹게 빨리 와주세요';
+
   final cartController = Get.put(CartController());
   //일단 controller가 디펜던시 인젝션 되면 다시 생성할 필요 없음
   @override
@@ -20,7 +28,7 @@ class _onairdocState extends State<onairdoc> {
     return Scaffold(
       backgroundColor: Color(0xff326295),
       body: Row(
-        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
@@ -68,6 +76,40 @@ class _onairdocState extends State<onairdoc> {
               ],
             ),
           ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 100,
+                ),
+                Container(
+                  child: Text(name,
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                ),
+                Container(
+                  child: Text(place1,
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                ),
+                Container(
+                  child: Text(place2,
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                ),
+                Container(
+                  child: Text(temName,
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                ),
+                Container(
+                  child: Text(hope,
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                ),
+                Container(
+                  child: Text(price,
+                      style: TextStyle(color: Colors.white, fontSize: 20)),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
